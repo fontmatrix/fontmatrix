a162b40ece48b2aece1f6161974e1d60f3d3ee80
Riku Leino <riku@scribus.info>
2007-12-11 04:07:42 +0000

    Added Finnish translation file and the first few translated messages.

---
+++ src/messages/fontmatrix_fi.ts
@@ -0,0 +1,541 @@
+<!DOCTYPE TS>
+<TS>
+  <context>
+    <name>About</name>
+    <message>
+      <source>About Fontmatrix</source>
+      <translation>Tietoja Fontmatrixista</translation>
+    </message>
+  </context>
+  <context>
+    <name>BookExportDialog</name>
+    <message>
+      <source>Dialog</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Font book options </source>
+      <translation>Fonttiesittelyn asetukset</translation>
+    </message>
+    <message>
+      <source>File</source>
+      <translation>Tiedosto</translation>
+    </message>
+    <message>
+      <source>Browse...</source>
+      <translation>Selaa...</translation>
+    </message>
+    <message>
+      <source>Paper Size</source>
+      <translation>Paperin koko</translation>
+    </message>
+    <message>
+      <source>X-Pos</source>
+      <translation>X-sijainti</translation>
+    </message>
+    <message>
+      <source>Family </source>
+      <translation>Perhe </translation>
+    </message>
+    <message>
+      <source>Style </source>
+      <translation>Tyyli </translation>
+    </message>
+    <message>
+      <source>Text</source>
+      <translation>Teksti</translation>
+    </message>
+    <message>
+      <source>Font size</source>
+      <translation>Fonttikoko</translation>
+    </message>
+    <message>
+      <source>Family</source>
+      <translation>Perhe</translation>
+    </message>
+    <message>
+      <source>Style</source>
+      <translation>Tyyli</translation>
+    </message>
+    <message>
+      <source>Headline</source>
+      <translation>Otsikko</translation>
+    </message>
+    <message>
+      <source>Body</source>
+      <translation>Leipäteksti</translation>
+    </message>
+    <message>
+      <source>Sample text</source>
+      <translation>Esimerkkiteksti</translation>
+    </message>
+  </context>
+  <context>
+    <name>FontAction</name>
+    <message>
+      <source>Form</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>action</source>
+      <translation type="unfinished" >toiminto</translation>
+    </message>
+    <message>
+      <source>Add Tag</source>
+      <translation>Lisää merkintä</translation>
+    </message>
+  </context>
+  <context>
+    <name>Help</name>
+    <message>
+      <source>Fontmatrix Help</source>
+      <translation>Fontmatrixin ohje</translation>
+    </message>
+  </context>
+  <context>
+    <name>ImportedFonts</name>
+    <message>
+      <source>Dialog</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Imported fonts</source>
+      <translation>Tuodut fontit</translation>
+    </message>
+  </context>
+  <context>
+    <name>ListDock</name>
+    <message>
+      <source>Form</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Search through font informations</source>
+      <translation>Etsi fonttien tiedoista</translation>
+    </message>
+    <message>
+      <source>Search</source>
+      <translation>Etsi</translation>
+    </message>
+    <message>
+      <source>Respect entered text case when searching</source>
+      <translation>Etsintä on merkkikokoherkkä</translation>
+    </message>
+    <message>
+      <source>Case sensitive</source>
+      <translation>Merkkikokoherkkä</translation>
+    </message>
+    <message>
+      <source>Tag</source>
+      <translation>Merkintä</translation>
+    </message>
+    <message>
+      <source>Filter view by chosing tag</source>
+      <translation>Suodata merkinnällä</translation>
+    </message>
+    <message>
+      <source>TagSet</source>
+      <translation>Kokoelma</translation>
+    </message>
+    <message>
+      <source>Show only fonts that belong to chosen tagset</source>
+      <translation>Näytä ainoastaan fontit jotka kuuluvat valittuun kokoelmaan</translation>
+    </message>
+    <message>
+      <source>Names</source>
+      <translation>Nimet</translation>
+    </message>
+    <message>
+      <source>Files</source>
+      <translation>Tiedostot</translation>
+    </message>
+    <message>
+      <source>Preview</source>
+      <translation>Esikatselu</translation>
+    </message>
+    <message>
+      <source>Show complete collection of available fonts</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>View all</source>
+      <translation>Näytä kaikki</translation>
+    </message>
+    <message>
+      <source>Show all activated fonts</source>
+      <translation>Näyttä kaikki aktivoidut fontit</translation>
+    </message>
+    <message>
+      <source>View activated</source>
+      <translation>Näytä aktivoidut</translation>
+    </message>
+  </context>
+  <context>
+    <name>MainView</name>
+    <message>
+      <source>main view widget</source>
+      <translation>pääikkuna</translation>
+    </message>
+    <message>
+      <source>Right mouse click on a tag to add it or remove it from TagSet</source>
+      <translation>Poista ja lisää merkintä kokoelmasta oikealla hiiren painikkeella</translation>
+    </message>
+    <message>
+      <source>Font Info</source>
+      <translation>Fontin tiedot</translation>
+    </message>
+    <message>
+      <source>&lt;html>&lt;head>&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; />&lt;style type=&quot;text/css&quot;>
+p, li { white-space: pre-wrap; }
+&lt;/style>&lt;/head>&lt;body style=&quot; font-family:'Sans Serif'; font-size:9pt; font-weight:400; font-style:normal;&quot;>
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;>&lt;/p>&lt;/body>&lt;/html></source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Sample text</source>
+      <translation>Esimerkkiteksti</translation>
+    </message>
+    <message>
+      <source>Fit to view</source>
+      <translation>Sovita alueelle</translation>
+    </message>
+    <message>
+      <source>Zoom is disabled, uncheck fit to view to enable zoom</source>
+      <translation>Koon muutos on pois käytöstä. Saat sen käyttöön poistamalla Sovita alueelle käytöstä</translation>
+    </message>
+    <message>
+      <source>FT</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>OpenType Features</source>
+      <translation>OpenType-ominaisuudet</translation>
+    </message>
+    <message>
+      <source>Friendly Name</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Apply features</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Glyphs</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Coverage of unicode interval :</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Clicking on a glyph will show details about it</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Tags</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+  <context>
+    <name>PrefsPanel</name>
+    <message>
+      <source>Preferences Panel</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>General</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Show Fontmatrix in system tray</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Close to system tray</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Show Activate all and Deactivate all in system tray menu</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Ask confirmation when activating or deactivating all fonts</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Ask confirmation when activating or deactivating tags</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Preview word</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Sample text</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Named sample texts</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Add</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Validate</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Layout</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Font size </source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source> pt</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Interline </source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Apply</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+  <context>
+    <name>PrefsPanelDialog</name>
+    <message>
+      <source>Looks like your setup does not have a system tray available.</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>A text</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+  <context>
+    <name>QObject</name>
+    <message>
+      <source>codepoint is U+</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+  <context>
+    <name>Systray</name>
+    <message>
+      <source>&amp;Activate all</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>&amp;Deactivate all</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Mi&amp;nimize</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>&amp;Restore</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>&amp;Exit</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>&amp;Tags</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+  <context>
+    <name>TagSetDialog</name>
+    <message>
+      <source>TagSet editor</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Collections of Tags</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Available tags</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Add selected tag to current set.</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>add</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Collections</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>New</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>delete</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>remove</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Close</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+  <context>
+    <name>typotek</name>
+    <message>
+      <source>Fontmatrix</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>The program will keep running in the system tray. To terminate the program, choose &lt;b>Exit&lt;/b> in the context menu of the system tray entry.</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Add Directory</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>&amp;Import...</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Ctrl+O</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Import a directory</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>&amp;Save</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Ctrl+S</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Save the document to disk</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Print...</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Print a specimen of the current font</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Export font book...</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Export a pdf that show selected fonts</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>E&amp;xit</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Ctrl+Q</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Exit the application</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>&amp;About</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Show the Typotek's About box</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Help</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>&amp;Tag Sets</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Activate all currents</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Deactivate all currents</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Edit current font</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Try to run Fontforge with the selected font as argument</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Preferences</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>&amp;File</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Edit</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>&amp;Help</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Ready</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Print specimen</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>There is no font selected</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Support of DragNDrop over http is sheduled but not yet effective</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>You bring something over me I canât handle</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>You provided an empty string, itâs not fair</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>&quot;default&quot; is a reserved keyword</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+</TS>

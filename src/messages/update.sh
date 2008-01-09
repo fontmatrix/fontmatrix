405e5c2839a7bd5ec9d2ec04284e5cb07a9791e4
Pierre Marchand <pierremarc@oep-h.com>
2008-01-09 22:16:19 +0000

    Fixed a typo in update script for TS files

--- src/messages/update.sh
+++ src/messages/update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Update TS files without PRO file and waiting for WRAP_TS macro in CMake 
-ls ../*.h >> lupdate.files
+ls ../*.h > lupdate.files
 ls ../*.cpp >> lupdate.files
 ls ../*.ui >> lupdate.files
 for i in *.ts; do lupdate `cat lupdate.files ` -ts "$i" ; done
